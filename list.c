3b976ce9c03fc7b701ec778d58b97cec9c0e5fca
Chidambaram Annamalai <quantumelixir@gmail.com>
2011-01-22 12:30:25 +0530

    Fix compiler warnings

--- list.c
+++ list.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#include <malloc.h>
-
 #include "list.h"
 
 /*
--- list.h
+++ list.h
@@ -1,6 +1,8 @@
 #ifndef __LIST_H__
 #define __LIST_H__
 
+#include <stdlib.h>
+
 typedef struct Node Node; 
 struct Node {
     void* data;
